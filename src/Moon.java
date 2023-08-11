3bcea57961efbf68fe5b757ec4253c2666d490e6
Testerek123 <lh.seller1.1.test@gmail.com>
2023-08-11 14:00:04 +0200

    test

--- src/Moon.java
+++ src/Moon.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Moon {
+public class Moon {
+    public static void main(String[] args) {
+    String word = "Hello Łukasz";
+
+        System.out.println(word);
+    }
 }
+
+
