637be1c99758e1b25ff477e5e775608e6f5ce3af
Testerek123 <lh.seller1.1.test@gmail.com>
2023-08-11 14:11:04 +0200

    calc

--- src/Moon.java
+++ src/Moon.java
@@ -1,8 +1,13 @@
 public class Moon {
     public static void main(String[] args) {
-    String word = "Hello Łukasz";
+        String word = "Hello Łukasz";
 
         System.out.println(word);
+
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
 
